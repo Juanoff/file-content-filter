fb70cf296986936e223671efbb66142b637ab047
juanoff <sasagrigorov999@gmail.com>
2025-08-27 22:20:40 +0700

    refactor: rename fields and update their descriptions

--- src/main/java/edu/juanoff/FileContentFilter.java
+++ src/main/java/edu/juanoff/FileContentFilter.java
@@ -8,35 +8,61 @@ import picocli.CommandLine.Parameters;
 import java.io.*;
 import java.util.concurrent.Callable;
 
-@Command(name = "file_content_filter",
+@Command(
+        name = "file-content-filter",
         mixinStandardHelpOptions = true,
-        version = "file_content_filter 1.0",
-        description = "Filtering content of files to output files and prints statistics in STDOUT.")
+        version = "file-content-filter 1.0",
+        description = "Filters content of input files to output files, " +
+                "write results to output files and prints statistics to STDOUT."
+)
 class FileContentFilter implements Callable<Integer> {
 
-    @Parameters(paramLabel = "FILENAME", description = "One or more files whose need to filtering.", arity = "1..*")
-    private String[] fileNames;
+    @Parameters(
+            paramLabel = "FILES",
+            description = "One or more input files to filter.",
+            arity = "1..*"
+    )
+    private String[] inputFiles;
 
-    @Option(names = {"-o"}, paramLabel = "DIRECTORY", description = "Directory to save output files.", defaultValue = "")
-    private String saveDirectory;
+    @Option(
+            names = {"-o", "--output-dir"},
+            paramLabel = "DIRECTORY",
+            description = "Directory where output files will be saved. Defaults to current directory.",
+            defaultValue = ""
+    )
+    private String outputDir;
 
-    @Option(names = {"-s"}, description = "Short statistics.")
-    private boolean shortStatistics;
+    @Option(
+            names = {"-s", "--short-stats"},
+            description = "Print short statistics."
+    )
+    private boolean shortStats;
 
-    @Option(names = {"-f"}, description = "Full statistics.")
-    private boolean fullStatistics;
+    @Option(
+            names = {"-f", "--full-stats"},
+            description = "Print full statistics."
+    )
+    private boolean fullStats;
 
-    @Option(names = {"-p"}, paramLabel = "PREFIX", description = "Prefix name for output files.", defaultValue = "")
-    private String prefix;
+    @Option(
+            names = {"-p", "--prefix"},
+            paramLabel = "PREFIX",
+            description = "Prefix for naming output files.",
+            defaultValue = ""
+    )
+    private String filePrefix;
 
-    @Option(names = {"-a"}, description = "Activate append mode.")
-    private boolean append;
+    @Option(
+            names = {"-a", "--append"},
+            description = "Enable append mode (append results to existing output files instead of overwriting)."
+    )
+    private boolean appendMode;
 
     @Override
     public Integer call() throws Exception {
-        BufferedWriter writer = new BufferedWriter(new FileWriter(saveDirectory + prefix + "integers.txt", append));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outputDir + filePrefix + "integers.txt", appendMode));
 
-        for (String fileName : fileNames) {
+        for (String fileName : inputFiles) {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
             String line = reader.readLine();
 
