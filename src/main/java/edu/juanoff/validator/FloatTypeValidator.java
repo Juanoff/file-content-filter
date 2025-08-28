3befb1d667e2864e889932cca3e641a887ce465c
juanoff <sasagrigorov999@gmail.com>
2025-08-29 06:40:26 +0700

    refactor: improve validation process for float

--- src/main/java/edu/juanoff/validator/FloatTypeValidator.java
+++ src/main/java/edu/juanoff/validator/FloatTypeValidator.java
@@ -2,15 +2,34 @@ package edu.juanoff.validator;
 
 import edu.juanoff.OutputFileName;
 
+import java.math.BigDecimal;
+
 public class FloatTypeValidator implements TypeValidator {
+    private final TypeValidator integerValidator = new IntegerTypeValidator();
+    private static final BigDecimal MAX_FLOAT = new BigDecimal(String.valueOf(Double.MAX_VALUE));
+    private static final int MAX_SIGNIFICANT_DIGITS = 17;
+
     @Override
     public boolean isValid(String value) {
         if (value == null || value.isEmpty()) {
             return false;
         }
         try {
-            Double.parseDouble(value.trim());
-            return value.contains(".");
+            double doubleValue = Double.parseDouble(value.trim());
+            if (Double.isInfinite(doubleValue) || Double.isNaN(doubleValue)) {
+                return false;
+            }
+
+            BigDecimal val = new BigDecimal(value.trim());
+            if (val.abs().compareTo(MAX_FLOAT) > 0) {
+                return false;
+            }
+
+            int significantDigits = val.stripTrailingZeros().precision();
+            if (significantDigits > MAX_SIGNIFICANT_DIGITS) {
+                return false;
+            }
+            return !integerValidator.isValid(value);
         } catch (NumberFormatException ex) {
             return false;
         }
